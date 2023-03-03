943337ea2286b93d6109a0ba5dd999d869dc8095
orib <oribrau@gmail.com>
2023-03-03 14:40:05 +0200

    disable console in prod fix header menu

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,20 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
+    disableConsole();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
+
+function disableConsole() {
+    window.console.log = () => {};
+    window.console.assert = () => {};
+    window.console.dir = () => {};
+    // window.console.error = () => {};
+    window.console.info = () => {};
+    window.console.time = () => {};
+    window.console.timeEnd = () => {};
+    window.console.trace = () => {};
+    window.console.warn = () => {};
+}
